0083b82ec21e7cc7917ab25b4d54621a30f22955
Leo Chen <chenl3@rose-hulman.edu>
2021-11-01 10:51:09 -0700

    commit tree 42311609a49fe6924ae8963fe3d402d1a2037235

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,38 @@
 def admin_login(username, password)
-  # your code here
+  if (username.downcase == 'admin' && password == '12345')
+    'Access granted'
+  else
+    'Access denied'
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40 
+    'It\'s brisk out there!'
+  elsif temperature <= 65
+    'It\'s a little chilly out there!'
+  elsif temperature <= 85
+    'It\'s perfect out there!'
+  else
+    'It\'s too dang hot out there!'
+  end
 end
 
+
 def fizzbuzz(num)
-  # your code here
+  result = ''
+  result += 'Fizz' if num % 3 == 0
+  result += 'Buzz' if num % 5 == 0
+  result = num if result.length == 0
+  result
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when '+' then num1 + num2
+  when '-' then num1 - num2
+  when '*' then num1 * num2
+  when '/' then num1 / num2
+  else puts 'Invalid operation!'
+  end
 end
-
